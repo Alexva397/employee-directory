31ba8f06a49a4590633a433c3bce76b2a84353eb
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-06-17 18:30:19 -0500

    created page title, deleted react starter files

---
+++ employeedirectory/src/components/Title.js
@@ -0,0 +1,13 @@
+function Title() {
+    return (
+        <>
+        <div className="bg-light p-4 rounded-lg text-center">
+            <h1 className="display-4">Employee Directory</h1>
+            <p className="lead">Search by empoyee or apply a filter to sort the companies employees</p>
+        </div>
+        <hr />
+        </>
+    );
+}
+
+export default Title;
